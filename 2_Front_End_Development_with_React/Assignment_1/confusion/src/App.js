dde99361788d2468550d392e88d40f85d291ba12
Dominik Söllinger <dominik.soellinger@stud.sbg.ac.at>
2019-09-19 14:42:19 +0200

    Components Part 2

--- 2_Front_End_Development_with_React/Assignment_1/confusion/src/App.js
+++ 2_Front_End_Development_with_React/Assignment_1/confusion/src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,7 +21,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
