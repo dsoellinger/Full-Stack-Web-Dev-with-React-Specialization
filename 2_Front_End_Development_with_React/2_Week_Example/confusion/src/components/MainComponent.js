dbd63688610483a1590a3c957d5418b2eca14fb6
Dominik Söllinger <dominik.soellinger@stud.sbg.ac.at>
2019-10-27 10:35:51 +0100

    Single Page Applications Part 1

--- 2_Front_End_Development_with_React/2_Week_Example/confusion/src/components/MainComponent.js
+++ 2_Front_End_Development_with_React/2_Week_Example/confusion/src/components/MainComponent.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import { DISHES } from '../shared/dishes';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactComponent';
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -12,6 +16,9 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS
     };
   }
 
@@ -19,7 +26,9 @@ class Main extends Component {
 
     const HomePage = () => {
       return (
-        <Home />
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
       );
     }
 
@@ -31,6 +40,7 @@ class Main extends Component {
 
           { /* If "exact" is not set, "/menu/xy" will match */ }
           <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes} /> } />
+          <Route exact path="/contactus" component={Contact} />
 
           <Redirect to="/home" />
         </Switch>
