2a8f973a30fcc19b009e15c476f5bba42ba822df
Dominik Söllinger <dominik.soellinger@stud.sbg.ac.at>
2019-10-27 16:43:56 +0100

    Assignment 2

---
+++ 2_Front_End_Development_with_React/2_Assignment/confusion/src/components/MenuComponent.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+function RenderMenuItem( {dish, onClick} ) {
+  return (
+    <Card>
+      <Link to={`/menu/${dish.id}`}>
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardImgOverlay>
+          <CardTitle>{dish.name}</CardTitle>
+        </CardImgOverlay>
+      </Link>
+    </Card>
+  );
+}
+
+const Menu = (props) => {
+
+  const menu = props.dishes.map((dish) => {
+    return (
+      <div className="col-12 col-md-5 m-1"  key={dish.id}>
+        <RenderMenuItem dish={dish} onClick={props.onClick} />
+      </div>
+    );
+  });
+
+  return (
+    <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+          <BreadcrumbItem active>Menu</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>Menu</h3>
+          <hr />
+        </div>
+      </div>
+      <div className="row">
+        {menu}
+      </div>
+    </div>
+  );
+
+}
+
+export default Menu;
