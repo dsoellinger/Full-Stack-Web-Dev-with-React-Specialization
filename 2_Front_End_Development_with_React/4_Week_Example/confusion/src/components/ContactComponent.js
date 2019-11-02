9d56266aca7a219fa62f6c942fde9460c2f4d66b
Dominik Söllinger <dominik.soellinger@stud.sbg.ac.at>
2019-11-02 09:54:30 +0100

    React Redux Forms Revisited

--- 2_Front_End_Development_with_React/4_Week_Example/confusion/src/components/ContactComponent.js
+++ 2_Front_End_Development_with_React/4_Week_Example/confusion/src/components/ContactComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem, Button, Row, Col, Label } from 'reactstrap';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors, action } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 
 const required = (val) => val && val.length;
@@ -20,6 +20,7 @@ class Contact extends Component {
   handleSubmit(values) {
     console.log("Current State is: " + JSON.stringify(values));
     alert("Current State is: " + JSON.stringify(values));
+    this.props.resetFeedbackForm();
   }
 
   render() {
@@ -67,7 +68,7 @@ class Contact extends Component {
             <h3> Send us Yor Feedback</h3>
           </div>
           <div className="col-12 col-md-9">
-            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+            <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
               <Row className="form-group">
                 <Label htmlFor="firstname" md={2}>First Name</Label>
                 <Col md={10}>
@@ -150,7 +151,7 @@ class Contact extends Component {
                   </Button>
                 </Col>
               </Row>
-            </LocalForm>
+            </Form>
           </div>
         </div>
       </div>
--- 2_Front_End_Development_with_React/4_Week_Example/confusion/src/components/MainComponent.js
+++ 2_Front_End_Development_with_React/4_Week_Example/confusion/src/components/MainComponent.js
@@ -9,7 +9,7 @@ import About from './AboutComponent';
 import { addComment, fetchDishes } from '../redux/ActionCreators';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-
+import { actions } from 'react-redux-form';
 
 const mapStateToProps = state => {
   return {
@@ -22,7 +22,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => ({
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => {dispatch(fetchDishes())}
+  fetchDishes: () => {dispatch(fetchDishes())},
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
 });
 
 
@@ -62,7 +63,7 @@ class Main extends Component {
           { /* If "exact" is not set, "/menu/xy" will match */ }
           <Route exact path="/menu" component={ () => <Menu dishes={this.props.dishes} /> } />
           <Route path="/menu/:dishId" component={DishWithId} />
-          <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
           <Route exact path="/aboutus" component={ () => <About leaders={this.props.leaders} /> } />
           <Redirect to="/home" />
         </Switch>
