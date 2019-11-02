a0ac72869c48572fc6fc313d9138e7c7cf8c3682
Dominik Söllinger <dominik.soellinger@stud.sbg.ac.at>
2019-11-02 14:12:48 +0100

    React Animation Components

--- 2_Front_End_Development_with_React/4_Week_Example/confusion/src/components/DishDetailComponent.js
+++ 2_Front_End_Development_with_React/4_Week_Example/confusion/src/components/DishDetailComponent.js
@@ -4,16 +4,22 @@ import { Link } from 'react-router-dom';
 import CommentForm from './CommentFormComponent';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components'
 
 function RenderDish( {dish} ) {
   return (
-    <Card>
-      <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-      <CardBody>
-        <CardTitle>{dish.name}</CardTitle>
-        <CardText>{dish.description}</CardText>
-      </CardBody>
-    </Card>
+    <FadeTransform in
+      transformProps={{
+        exitTransform: 'scale(0.5) translateY(-50%)'
+      }}>
+      <Card>
+        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    </FadeTransform>
   );
 }
 
@@ -25,14 +31,22 @@ function RenderComments( {comments, dishId, postComment} ) {
     );
   }
 
-  const renderedComments = comments.map((comment) => {
-    return (
-      <li key={comment.id}>
-        <p>{comment.comment}</p>
-        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-      </li>
-    );
-  });
+  const renderedComments = (
+    <Stagger in>
+      {
+        comments.map((comment) => {
+          return (
+            <Fade in>
+              <li key={comment.id}>
+                <p>{comment.comment}</p>
+                <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+              </li>
+            </Fade>
+          );
+        })
+      }
+    </Stagger>
+  );
 
   return (
     <React.Fragment>
