88860b1f904adec9a12af0416c44b3bf7da2aba4
Dominik Söllinger <dominik.soellinger@stud.sbg.ac.at>
2019-11-19 22:20:06 +0100

    Node Callbacks and Error Handling

--- 3_Server-side_development_with_NodeJS/1_Week_Example/NodeJS/node-examples/index.js
+++ 3_Server-side_development_with_NodeJS/1_Week_Example/NodeJS/node-examples/index.js
@@ -1,16 +1,22 @@
 var rect = require('./rectangle');
 
 function solveRect(l,b) {
-	console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
-	if ( l <= 0 || b <= 0) {
-		console.log("Rectangle dimensions should be greater than zero: l = " + l + ", and b = " + b);
-	}
-	else {
-		console.log("The area of the rectangle is " + rect.area(l,b));
-		console.log("The perimeter of the rectangel is " + rect.perimeter(l,b));
-	}
-}
+		console.log("Solving for rectangle with l = " + l + " and b = " + b);
+
+    rect(l, b, (err,rectangle) => {
+    	if (err) {
+				console.log("ERROR: ", err.message);
+			}
+			else {
+				console.log("The area of the rectangle of dimensions l = " + l + " and b = " + b + " is " + rectangle.area());
+				console.log("The perimeter of the rectangle of dimensions l = " + l + " and b = " + b + " is " + rectangle.perimeter());
+			}
+  	});
+
+  	console.log("This statement after the call to rect()");
+
+};
 
 solveRect(2,4);
 solveRect(3,5);
--- 3_Server-side_development_with_NodeJS/1_Week_Example/NodeJS/node-examples/rectangle.js
+++ 3_Server-side_development_with_NodeJS/1_Week_Example/NodeJS/node-examples/rectangle.js
@@ -1,2 +1,7 @@
-exports.perimeter =  (x, y) => (2*(x+y));
-exports.area = (x, y) => (x*y);
+module.exports = (x, y, callback) => {
+  if (x <= 0 || y <= 0)
+    setTimeout(() => callback(new Error("Rectangle dimensions should be greater than zero: l = " + x + ", and b = " + y), null), 2000);
+  else
+    setTimeout(() => callback(null, { perimeter: () => (2*(x+y)), area:() => (x*y) }), 2000);
+
+}
